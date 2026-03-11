934e05013aaff210d75554ee3d8c4a09a8ecdd06
rlaehddud475 <6070kjh475@naver.com>
2026-03-11 11:41:53 +0900

    .

--- third/third/third.c
+++ third/third/third.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main(void){
-    printf("%d / %d = %f",100,200, 0.5);
-    printf("\n");
+    printf("%d / %d = %f\n",100,200, 0.5);
+    printf("%c %c \n",'a', 'K');
+    printf("%s %s \n","안녕","C 언어");
 }
