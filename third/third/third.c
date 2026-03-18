1200a7894b390c648e1e303987b7001b68bddb29
rlaehddud475 <6070kjh475@naver.com>
2026-03-18 10:28:13 +0900

    .

--- third/third/third.c
+++ third/third/third.c
@@ -16,10 +16,22 @@ int main(void){
     //    printf("\a\a\a삐소리 3번\n");
     //    printf("글자가 \"강조\" 되는 효과\n");
     //    printf("\\\\\\ 역슬래시 세 개 출력\n");
-    int a;
-    float b;
-    a=123.45;
-    b=200;
-    printf("a의 값은 ==> %d\n",a);
-    printf("b의 값은 ==> %f\n",b);
+//    int a;
+//    float b;
+//    a=123.45;
+//    b=200;
+//    printf("a의 값은 ==> %d\n",a);
+//    printf("b의 값은 ==> %f\n",b);
+    int a,b,c,d;
+    a=100+100;
+    b=a+100;
+    c=a+b-100;
+    d=a+b+c;
+    printf("a, b, c, d 의 값 ==> %d,%d,%d,%d\n", a,b,c,d);
+    a=b=c=d=100;
+    printf("a, b, c, d 의 값 ==> %d,%d,%d,%d\n", a,b,c,d);
+    a=100;
+    a=a+200;
+    printf("a의 값 ==> %d\n", a);
+    
 }
