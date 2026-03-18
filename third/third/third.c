acf68e6ba602d691c184f78f8361008e1ac97b32
rlaehddud475 <6070kjh475@naver.com>
2026-03-18 09:42:32 +0900

    .

--- third/third/third.c
+++ third/third/third.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 int main(void){
-    printf("%d / %d = %f\n",100,200, 0.5);
-    printf("%c %c \n",'a', 'K');
-    printf("%s %s \n","안녕","C 언어");
+//    printf("%d\n",123);
+//    printf("%5d\n",123);
+//    printf("%05d\n",123);
+//    
+//    printf("%f\n",123.45);
+//    printf("%7.1f\n",123.45);
+//    printf("%7.3f\n",123.45);
+//    
+//    printf("%s\n","Basic-C");
+//    printf("%10s\n","Basic-C")
+    printf("\n줄바꿈\n연습\n");
+    printf("\t탭키\t연습\n");
+    printf("이것을\r덮어씁니다 \n");
+    printf("\a\a\a삐소리 3번\n");
+    printf("글자가 \"강조\" 되는 효과\n");
+    printf("\\\\\\ 역슬래시 세 개 출력\n");
 }
