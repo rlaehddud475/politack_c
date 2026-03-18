6c353569649fe0537b220e711c4ccd84ea69a71c
rlaehddud475 <6070kjh475@naver.com>
2026-03-18 10:10:57 +0900

    .

--- third/third/third.c
+++ third/third/third.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 int main(void){
-//    printf("%d\n",123);
-//    printf("%5d\n",123);
-//    printf("%05d\n",123);
-//    
-//    printf("%f\n",123.45);
-//    printf("%7.1f\n",123.45);
-//    printf("%7.3f\n",123.45);
-//    
-//    printf("%s\n","Basic-C");
-//    printf("%10s\n","Basic-C")
-    printf("\n줄바꿈\n연습\n");
-    printf("\t탭키\t연습\n");
-    printf("이것을\r덮어씁니다 \n");
-    printf("\a\a\a삐소리 3번\n");
-    printf("글자가 \"강조\" 되는 효과\n");
-    printf("\\\\\\ 역슬래시 세 개 출력\n");
+    //    printf("%d\n",123);
+    //    printf("%5d\n",123);
+    //    printf("%05d\n",123);
+    //
+    //    printf("%f\n",123.45);
+    //    printf("%7.1f\n",123.45);
+    //    printf("%7.3f\n",123.45);
+    //
+    //    printf("%s\n","Basic-C");
+    //    printf("%10s\n","Basic-C")
+    //    printf("\n줄바꿈\n연습\n");
+    //    printf("\t탭키\t연습\n");
+    //    printf("이것을\r덮어씁니다 \n");
+    //    printf("\a\a\a삐소리 3번\n");
+    //    printf("글자가 \"강조\" 되는 효과\n");
+    //    printf("\\\\\\ 역슬래시 세 개 출력\n");
+    int a;
+    float b;
+    a=123.45;
+    b=200;
+    printf("a의 값은 ==> %d\n",a);
+    printf("b의 값은 ==> %f\n",b);
 }
