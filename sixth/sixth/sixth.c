2069b812dae48d44d5598fbebf6f4bd9c0327e40
rlaehddud475 <6070kjh475@naver.com>
2026-04-08 09:15:13 +0900

    .

--- sixth/sixth/sixth.c
+++ sixth/sixth/sixth.c
@@ -2,6 +2,11 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
+int main(void) {
+   
     
+    int i;
+    for (i=0; i<5; i++) {
+        printf("안녕하세요? 빙글빙글 for문울 공부 중입니다 \n");
+    }
 }
