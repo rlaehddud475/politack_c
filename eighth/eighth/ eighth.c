c9ce0d66644eec4fa5195b9912334adb9ffee05a
rlaehddud475 <6070kjh475@naver.com>
2026-04-29 10:09:07 +0900

    .

--- eighth/eighth/ eighth.c
+++ eighth/eighth/ eighth.c
@@ -1,15 +1,17 @@
-//
-//  main.c
-//  eighth
-//
-//  Created by 김동영 on 4/29/26.
-//
 
-#include <stdlib.h>
+
+
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+    //    int sum = 0;
+    //    int n[4],i;
+    //
+    //    for(i=0; i<=3; i++){
+    //        printf("%d번째 숫자를 입력하세요 : ",i+1);
+    //        scanf("%d",&n[i]);
+    //        sum+=n[i];
+    //    }
+    //    printf("합계 ==> %d\n",sum);
+        
 }
