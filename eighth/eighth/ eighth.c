6178d8bc7cdf42dc3248134cdcbaf0ace786f73e
rlaehddud475 <6070kjh475@naver.com>
2026-04-29 09:03:50 +0900

    .

---
+++ eighth/eighth/ eighth.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  eighth
+//
+//  Created by 김동영 on 4/29/26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
