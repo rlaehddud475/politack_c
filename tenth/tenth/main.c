1f44b4e5355de5d492df9b632d84860e1ee0d878
rlaehddud475 <6070kjh475@naver.com>
2026-05-20 10:17:38 +0900

    .

--- tenth/tenth/main.c
+++ tenth/tenth/main.c
@@ -1,15 +1,47 @@
-//
-//  main.c
-//  tenth
-//
-//  Created by 김동영 on 5/20/26.
-//
-
-#include <stdlib.h>
+#define _CRT_SECURE_NO_WARINGS
 #include <stdio.h>
-
+//int a=100;
+//int calc(int v1, int v2, int op){
+//    int result;
+//    switch (op) {
+//        case 1:
+//            result=v1+v2;
+//            break;
+//        case 2:
+//            result=v1-v2;
+//            break;
+//        case 3:
+//            result=v1*v2;
+//            break;
+//        case 4:
+//            result=v1/v2;
+//            break;
+//    }
+//    return result;
+//}
+//int plus(int v1,int v2){
+//    int result;
+//    result = v1+v2;
+//    return result;
+//}
+//void func1(void){
+//    int a =200;
+//    printf("func1()에서 a의 값 ==> %d\n",a);
+//}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+//    func1();
+//    printf("main()에서 a의 값 ==> %d \n",a);
+//    int res;
+//    int oper,a,b;
+//    printf("계산 입력(1:+,2:-,3:*,4:/): ");
+//    scanf("%d",&oper);
+//    printf("계산할 두 숫자를 입력 : ");
+//    scanf("%d,%d",&a,&b);
+//    res=calc(a,b, oper);
+//    printf("계산 결과는 : %d \n",res);
+//    int hap;
+//    
+//    hap=plus(100,200);
+//    printf("100과 200의 plus() 함수 결과값 : %d \n",hap);
 }
+
