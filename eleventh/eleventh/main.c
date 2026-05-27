5dda2068ed2e3461f238679162b99684b9ab9d13
rlaehddud475 <6070kjh475@naver.com>
2026-05-27 10:36:09 +0900

    .

--- eleventh/eleventh/main.c
+++ eleventh/eleventh/main.c
@@ -1,26 +1,75 @@
 #include <stdio.h>
 #define _CRT_SECURE_NO_WRANINGS
+#include <string.h>
+
 int main(int argc, const char * argv[]) {
-//    int a;
-//    float b;
-//    char ch;
-//    char s[20];
-//    
-//    printf("정수를 입력 : ");
-//    scanf("%d",&a);
-//    printf("실수를 입력 : ");
-//    scanf("%f",&b);
-//    printf("문자를 입력 : ");
-//    scanf(" %c",&ch);
-//    printf("문자열을 입력 : ");
-//    scanf("%s",s);
-//    
-//    printf("\n 정수의 10진수 ==> %d\n",a);
-//    printf("정수의 16진수 ==> %X\n",a);
-//    printf("정수의 8진수 ==> %o\n",a);
-//    printf("실수 ==> %10.3f\n",b);
-//    printf("실수(공학용) ==> %e\n",b);
-//    printf("문자 ==> %c\n",ch);
-//    printf("문자열 ==> %s\n",s);
+    //    int a;
+    //    float b;
+    //    char ch;
+    //    char s[20];
+    //
+    //    printf("정수를 입력 : ");
+    //    scanf("%d",&a);
+    //    printf("실수를 입력 : ");
+    //    scanf("%f",&b);
+    //    printf("문자를 입력 : ");
+    //    scanf(" %c",&ch);
+    //    printf("문자열을 입력 : ");
+    //    scanf("%s",s);
+    //
+    //    printf("\n 정수의 10진수 ==> %d\n",a);
+    //    printf("정수의 16진수 ==> %X\n",a);
+    //    printf("정수의 8진수 ==> %o\n",a);
+    //    printf("실수 ==> %10.3f\n",b);
+    //    printf("실수(공학용) ==> %e\n",b);
+    //    printf("문자 ==> %c\n",ch);
+    //    printf("문자열 ==> %s\n",s);
+    //    char password[5]="5678";
+    //    char input[5];
+    //    int i;
+    //
+    //    printf("비밀번호 4글자를 입력하세요 : ");
+    //    for (i=0; i<4; i++) {
+    //        input[i]=getchar();
+    //    }
+    //    if (strncmp(password, input, 4)==0) {
+    //        printf("\n비밀번호 통과 ~~~\n");
+    //    }else{
+    //        printf("\n입력한 비밀번호\n");
+    //        for (i=0; i<4; i++) {
+    //            putchar(input[i]);
+    //
+    //        }
+    //        printf(" 가  틀렸음\n");
+    //    }
+    //    char ch;
+    //    printf("문자열을 입력하세요 : ");
+    //    ch=getchar();
+    //    putchar(ch);
+    //
+    //    ch=getchar();
+    //    putchar(ch);
+    //
+    //    ch=getchar();
+    //    putchar(ch);
+    //    char s[20];
+    //    FILE *rfp;
+    //    rfp=fopen("/Users/propheter/CookC/eleventh/data1.txt", "r");
+    //
+    //    fgets(s, 20, rfp);
+    //    printf("파일에서 읽은 문자열 : ");
+    //    puts(s);
+    //
+    //    fclose(rfp);
+//    char str[200];
+//    FILE *rfp;
+//    rfp=fopen("/Users/propheter/CookC/eleventh/data2.txt", "r");
+//    for (;;) {
+//        fgets(str, 200, rfp);
+//        if(feof(rfp))
+//            break;
+//        printf("%s",str);
+//    }
+//    fclose(rfp);
     
 }
